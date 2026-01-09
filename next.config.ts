31a8313f3b90a1e01532ed2d804461fde695072a
skudoslin-endryx <skudoslin@gmail.com>
2026-01-09 10:19:40 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // 关键配置：告诉 Next.js 导出为纯静态 HTML 文件
+  output: "export",
+  
+  // 关闭图片优化服务 (因为静态网站没有后台服务器来处理图片压缩)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
